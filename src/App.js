f3a3a13154f5c7d6971842818f255a70be2cc281
Luca Zeni <luca_zeni@hotmail.com>
2020-06-08 23:55:02 -0400

    add registration for user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:8080');
+
+socket.on('connect', () => {
+  console.log('Successfully connected!');
+});
+
+function addUser() {
+  if (socket.connected) {
+  socket.emit('addUser', 'Luca');
+  }
+}
 
 function App() {
   return (
     <header className="App-header">
-    <form>
-  <label>
-    Nickname:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Enter" />
-</form>
-</header>
+        <label className="textBoxLabel">Nickname:</label>
+        <br></br>
+        <input className="textBox" type="text" name="name" />
+        <br></br>
+        <input className="button" onClick={addUser} type="button" value="Play" />
+    </header>
   );
 }
 
