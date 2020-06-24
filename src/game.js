c9b581d2aa56c6cc2e031f922ee35d1efeff4c6b
Luca Zeni <luca_zeni@hotmail.com>
2020-06-23 20:25:50 -0400

    working on game page

--- src/game.js
+++ src/game.js
@@ -7,14 +7,17 @@ export default class Game extends React.Component {
 
     render() {
         return (
+            <div>
+            <div className="gameroom-title">Room: {this.props.roomName}</div>
             <div className="pregame-container">
-             <div>{this.props.roomName}</div>
+           
                 <div className="game-container">
 
                 </div>
                 <div className="chat">
                     <input placeholder="Type a message.." className="chat-textbox"></input>
                 </div>
+            </div>
             </div>
         );
 
